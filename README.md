049228c5714816fdc43400c875dbbef13331398b
Bells307 <skyheart307@yandex.ru>
2022-12-28 00:31:16 +0300

    Добавлена документация по категориям

---
+++ README.md
@@ -0,0 +1,14 @@
+# adv-service
+# Configuration
+```bash
+# listen address
+LISTEN="0.0.0.0:10000"
+
+# mongodb configuration
+MONGODB.URI="mongodb://admin:admin@localhost:27017"
+MONGODB.DBNAME="adv-service"
+```
+
+# REST API
+https://petstore.swagger.io/?url=https://raw.githubusercontent.com/bells307/adv-service/master/docs/swagger.yaml
+
--- internal/infrastructure/delivery/http/v1/category.go
+++ internal/infrastructure/delivery/http/v1/category.go
@@ -31,6 +31,15 @@ func (h *categoryHandler) Register(g *gin.RouterGroup) {
 	}
 }
 
+//	@Summary	Создать категорию
+//	@Tags		category
+//	@ID			create-category
+//	@Accept		json
+//	@Produce	json
+//	@Param		input	body		usecase.CreateCategoryInput		true	"Создание объявления"
+//	@Success	201		{object}	usecase.CreateCategoryOutput	"Успешное создание категории"
+//	@Failure	500		{object}	err_resp.ErrorResponse			"Внутренняя ошибка сервиса"
+//	@Router		/api/v1/category [post]
 func (h *categoryHandler) createCategory(c *gin.Context) {
 	var input usecase.CreateCategoryInput
 	if err := c.Bind(&input); err != nil {
@@ -48,6 +57,13 @@ func (h *categoryHandler) createCategory(c *gin.Context) {
 	c.JSON(http.StatusCreated, gin.H{"id": out.ID})
 }
 
+//	@Summary	Удалить категорию
+//	@Tags		category
+//	@ID			delete-category
+//	@Param		id	query		string					false	"Идентификатор категории"
+//	@Success	204	{object}	string					"Удаление произведено успешно"
+//	@Failure	500	{object}	err_resp.ErrorResponse	"Внутренняя ошибка сервиса"
+//	@Router		/api/v1/category/{id} [delete]
 func (h *categoryHandler) deleteCategory(c *gin.Context) {
 	id := c.Param("id")
 	if len(id) == 0 {
